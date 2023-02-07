3f93bea0c13e7cc678b95090b5ed81d9f6b82026
Aidar06 <114663298+Aidar06@users.noreply.github.com>
2023-02-07 17:24:57 +0600

    mainHero (#5)

--- src/pages/Main/main.js
+++ src/pages/Main/main.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import MainCategory from "./MainCategory/mainCategory";
+import MainHero from "./MainHero/mainHero";
 
 const Main = () => {
     return (
         <div>
+            <MainHero/>
           <MainCategory/>
         </div>
     );
