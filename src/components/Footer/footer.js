1a3206c988ca74ecf6a2634f2f00efc971ba88d0
eldorado-22 <jumashevv992@gmail.com>
2023-02-07 15:07:14 +0600

    qwer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-import {BsInstagram, BsWhatsapp, FaFacebookF, FaTelegramPlane} from "react-icons/all";
+// import {BsInstagram, BsWhatsapp, FaFacebookF, FaTelegramPlane} from "react-icons/all";
 
 const Footer = () => {
     return (
