a2e8f9daf50402a620070e15a6c2070c6038a6c8
eldorado-22 <jumashevv992@gmail.com>
2023-02-17 09:59:50 +0600

    update

--- src/pages/Basket/basket.js
+++ src/pages/Basket/basket.js
@@ -122,7 +122,7 @@ const Basket = () => {
                                 return(
                                     <div onMouseMove={() => setNum(el.id)} className='basket--group__cloth' key={el.id}>
                                                   <div className='basket--group__cloth--img'>
-                                                      <img src={el.img} alt=""/>
+                                                      <img src={el.img} alt="img"/>
                                                   </div>
                                                   <div className='basket--group__cloth--info'>
                                                       <h1>{el.title}</h1>
