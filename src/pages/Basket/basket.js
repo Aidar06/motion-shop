1266693cf40b91c182b4a2da5d0c48f58b9f9b74
eldorado-22 <jumashevv992@gmail.com>
2023-02-26 15:18:04 +0600

    adaptation

--- src/pages/Basket/basket.js
+++ src/pages/Basket/basket.js
@@ -6,72 +6,77 @@ const Basket = () => {
     let clothes = JSON.parse(localStorage.getItem('clothes')) || []
 
     let check = true
-    function setCheck(){
-        if (clothes.length === 0){
+
+    function setCheck() {
+        if (clothes.length === 0) {
             check = false
-        }else {
+        } else {
             check = true
         }
     }
+
     setCheck()
 
-    const [date,setDate] = useState(true)
-    function apDate(){
+    const [date, setDate] = useState(true)
+
+    function apDate() {
         setDate(false)
     }
 
-    const [num,setNum] = useState(0)
-    function removeClothes(){
-           let clothes = JSON.parse(localStorage.getItem('clothes'))
+    const [num, setNum] = useState(0)
 
-           clothes = clothes.filter(el => {
-               return el.id !== num
-           })
-
-           let cloth = localStorage.setItem('clothes',JSON.stringify(clothes))
-           apDate()
-    }
-
-    function plusCount(){
-        let n = []
+    function removeClothes() {
         let clothes = JSON.parse(localStorage.getItem('clothes'))
 
-        clothes = clothes.map(el => {
-            if (el.id === num){
-                el.count += 1
-                el.price += el.firstPrice
-                n.push(el)
-            }else{
-                n.push(el)
-            }
+        clothes = clothes.filter(el => {
+            return el.id !== num
         })
 
-        clothes = n
-        let cloth = localStorage.setItem('clothes',JSON.stringify(clothes))
+        let cloth = localStorage.setItem('clothes', JSON.stringify(clothes))
         apDate()
     }
 
-    function minusCount(){
+    function plusCount() {
         let n = []
         let clothes = JSON.parse(localStorage.getItem('clothes'))
 
         clothes = clothes.map(el => {
-            if (el.id === num && el.count > 1){
-                el.count -= 1
-                el.price -= el.firstPrice
+            if (el.id === num) {
+                el.count += 1
+                el.price += el.firstPrice
                 n.push(el)
-            }else{
+            } else {
                 n.push(el)
             }
         })
 
         clothes = n
-        let cloth = localStorage.setItem('clothes',JSON.stringify(clothes))
+        let cloth = localStorage.setItem('clothes', JSON.stringify(clothes))
+        apDate()
+    }
+
+    function minusCount() {
+        let n = []
+        let clothes = JSON.parse(localStorage.getItem('clothes'))
+
+        clothes = clothes.map(el => {
+            if (el.id === num && el.count > 1) {
+                el.count -= 1
+                el.price -= el.firstPrice
+                n.push(el)
+            } else {
+                n.push(el)
+            }
+        })
+
+        clothes = n
+        let cloth = localStorage.setItem('clothes', JSON.stringify(clothes))
         apDate()
     }
 
     let newMoney = 0
-    function gerMoneyCount(){
+
+    function gerMoneyCount() {
         let clothes = JSON.parse(localStorage.getItem('clothes')) || []
 
         clothes.map(el => {
@@ -83,10 +88,11 @@ const Basket = () => {
 
     return (
         <section id='basket'>
-            <h4 style={{display: check?'':"none"}}>Оформление товара</h4>
+            <h4 style={{display: check ? '' : "none"}}>Оформление товара</h4>
             <div className="container">
-                <h1 className='basket-text' style={{display: check === false?'block':"none"}}>В корзине ничего не найдено!</h1>
-                <div className="basket" style={{display: check?'':"none"}}>
+                <h1 className='basket-text' style={{display: check === false ? 'block' : "none"}}>В корзине ничего не
+                    найдено!</h1>
+                <div className="basket" style={{display: check ? '' : "none"}}>
                     <div className="basket--tap">
                         <div className='basket--tap__nav'>
                             <NavLink to={''}>
@@ -97,7 +103,7 @@ const Basket = () => {
                                 <h2 className='basket--tap__nav--basket'>Что у меня в корзине?</h2>
                             </NavLink>
                         </div>
-                        <h3>Общая сумма:    <span>{newMoney} сом</span></h3>
+                        <h3>Общая сумма: <span>{newMoney} сом</span></h3>
                     </div>
 
                     <div className="basket--block">
@@ -121,7 +127,8 @@ const Basket = () => {
                                     <input type="radio" id='test'/>
                                     <h5>Доставка курьером</h5>
                                 </div>
-                                <input type="text" className='basket--block__group--del__ad' placeholder='Область, город (район, село), улица, дом№, кв.№*'/>
+                                <input type="text" className='basket--block__group--del__ad'
+                                       placeholder='Область, город (район, село), улица, дом№, кв.№*'/>
                             </div>
                         </div>
 
@@ -132,6 +139,8 @@ const Basket = () => {
                                     <input type="radio"/>
                                     <p>Оплачу наличными при получении <br/> заказа</p>
                                 </div>
+                                <button>Заказать</button>
+
                                 <div className="basket--block__group--pay__order">
                                     <input type="radio"/>
                                     <p>Оплата с банковской картой через <br/> <span>PayBox</span></p>
@@ -153,25 +162,27 @@ const Basket = () => {
                     <div className="basket--group">
                         {
                             date ? clothes.map(el => {
-                                return(
+                                return (
                                     <div onMouseMove={() => setNum(el.id)} className='basket--group__cloth' key={el.id}>
-                                                  <div className='basket--group__cloth--img'>
-                                                      <NavLink to={`/addBasket/${el.clothId}`} key={el.clothId}>
-                                                          <img src={el.img} alt=""/>
-                                                      </NavLink>
-                                                  </div>
-                                                  <div className='basket--group__cloth--info'>
-                                                      <h1>{el.title}</h1>
-                                                      <h4>Опции: <span>{el.size}</span>, <span>{el.color}</span></h4>
-                                                      <h2>{el.price} сом</h2>
-                                                      <div className='basket--group__cloth--info__count'>
-                                                          <div onClick={()=> minusCount()}>-</div>
-                                                          <p>{el.count}</p>
-                                                          <div onClick={()=> plusCount()}>+</div>
-                                                      </div>
-                                                      <h3 onClick={()=> {removeClothes()}}>Удалить</h3>
-                                                  </div>
-                                              </div>
+                                        <div className='basket--group__cloth--img'>
+                                            <NavLink to={`/addBasket/${el.clothId}`} key={el.clothId}>
+                                                <img src={el.img} alt=""/>
+                                            </NavLink>
+                                        </div>
+                                        <div className='basket--group__cloth--info'>
+                                            <h1>{el.title}</h1>
+                                            <h4>Опции: <span>{el.size}</span>, <span>{el.color}</span></h4>
+                                            <h2>{el.price} сом</h2>
+                                            <div className='basket--group__cloth--info__count'>
+                                                <div onClick={() => minusCount()}>-</div>
+                                                <p>{el.count}</p>
+                                                <div onClick={() => plusCount()}>+</div>
+                                            </div>
+                                            <h3 onClick={() => {
+                                                removeClothes()
+                                            }}>Удалить</h3>
+                                        </div>
+                                    </div>
                                 )
                             }) : setDate(true)
                         }
